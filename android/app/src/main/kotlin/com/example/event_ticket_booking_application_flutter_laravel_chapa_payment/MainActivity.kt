78f373cbf0c749157ccc607605f0bde47652c969
Medhin12 <medhin@360ground.com>
2024-02-02 20:52:25 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/event_ticket_booking_application_flutter_laravel_chapa_payment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.event_ticket_booking_application_flutter_laravel_chapa_payment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
